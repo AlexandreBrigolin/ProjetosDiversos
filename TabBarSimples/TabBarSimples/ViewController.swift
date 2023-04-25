f19119bbcdd025b1d7e61547093364053b5ffc6b
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-04-25 20:52:01 -0300

    adicionando projeto

---
+++ TabBarSimples/TabBarSimples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarSimples
+//
+//  Created by Alexandre Brigolin on 25/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
