186a3543e956cfc83c44c40f4f6c738ac212ba8a
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-04-25 22:09:59 -0300

    criando tela nova para a tabBar

---
+++ TabBarSimples/TabBarSimples/Tela03ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Tela03ViewController.swift
+//  TabBarSimples
+//
+//  Created by Alexandre Brigolin on 25/04/23.
+//
+
+import UIKit
+
+class Tela03ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Tela03")
+    }
+    
+
+
+}
