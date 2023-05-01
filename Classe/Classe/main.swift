bde14c2dcbf832d95a0cde6f595f3ca503bf89f7
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-05-01 16:50:19 -0300

    ciando classe e dando vida ao objeto

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+//Objeto
+//Caracteristicas de um objeto -> Atributos ou Propriedades
+//Acoes = funcoes e metodos
 
+class Automovel {
+    var portas: Int = 2
+    var nome: String = "Porshe"
+    var marca: String = "Ferrari"
+    
+    
+    func ligarCarro() {
+        print("O carro está ligado!")
+    }
+    
+    func ligarFarol() {
+        print("O farol está ligado!")
+    }
+}
+
+//instanciado a classe, toda vez que instacio uma classe quer dizer que estou dando via a esse objeto!
+var meuCarro: Automovel = Automovel()
