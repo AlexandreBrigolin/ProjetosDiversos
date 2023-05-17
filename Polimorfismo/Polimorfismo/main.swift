684d83ce023b7d9628c9d216b0dfe54590ab91e9
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-05-17 00:34:37 -0300

    trabalhando com polimorfismo

--- Polimorfismo/Polimorfismo/main.swift
+++ Polimorfismo/Polimorfismo/main.swift
@@ -10,15 +10,24 @@ import Foundation
 //MARK: POLIMORFISMO
 
 class Centauro {
-    func calculadora(valortotal: Double) -> Double {
-        return valortotal * 0.3
+    func totalDesconto(valorTotal: Double) -> Double {
+        return valorTotal * 0.3
     }
 }
 
-class Loja: Centauro {
-    
+class Loja:Centauro {
+    override func totalDesconto(valorTotal: Double) -> Double {
+        return valorTotal * 0.1
+
+    }
+}
+
+class Loja1:Centauro {
+
 }
 
 var lojaDoXandy: Loja = Loja()
+print(lojaDoXandy.totalDesconto(valorTotal: 1000))
 
-print(lojaDoXandy.calculadora(valortotal: 1000))
+var lojaDoCaio: Loja1 = Loja1()
+print(lojaDoCaio.totalDesconto(valorTotal: 1000))
