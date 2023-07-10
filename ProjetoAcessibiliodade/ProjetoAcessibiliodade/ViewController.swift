5966f69b0c44d2deb3303a5102393b9d59ea5b55
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-07-10 14:26:24 -0300

    criando projeto de acessibilidade

---
+++ ProjetoAcessibiliodade/ProjetoAcessibiliodade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjetoAcessibiliodade
+//
+//  Created by Alexandre Brigolin on 10/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
