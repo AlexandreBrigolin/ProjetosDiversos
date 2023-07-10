04a11527b90feecf56d9ea43e1bed1355e356c3c
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-07-10 14:23:36 -0300

    criando projeto

---
+++ TestesUnitarios/TestesUnitarios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestesUnitarios
+//
+//  Created by Alexandre Brigolin on 10/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
